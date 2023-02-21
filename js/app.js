af1a5ed9d200f480f6f536e398bbdc0cebcad37e
saidulcoder <isaidulislamsayed@gmail.com>
2023-02-22 00:33:43 +0600

    work done

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const loadPhotos = () => {
 
 const displayPhotos = photos=>{
     photos.forEach(photo => {
-        console.log(photo)
+        // console.log(photo)
         const allPhotoDiv = document.getElementById('photo-card');
         const photoDiv = document.createElement('div');
         photoDiv.innerHTML = `
@@ -16,13 +16,34 @@ const displayPhotos = photos=>{
           <div class="card-body">
             <h5 class="card-title">${photo.title}</h5>
             <p class="card-text">Album ID: ${photo.albumId}</p>
-            <button class="btn btn-primary" id="show-btn">Show Detials</button>
+            <button class="btn btn-primary" onclick = "loadPhotoDetials(${photo.id})">Show Detials</button>
           </div>
         </div>
       </div>
         `;
         allPhotoDiv.appendChild(photoDiv);
     });
+}
+
+const loadPhotoDetials = (id)=>{
+    const url = `https://jsonplaceholder.typicode.com/photos/${id}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayPhotoDetials(data))
+}
+
+const displayPhotoDetials = (photo)=>{
+    console.log(photo)
+    const photoDetials = document.getElementById('photo-details');
+    photoDetials.innerHTML = `
+    <div class="card w-50 mx-auto">
+    <img src="${photo.url}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${photo.title}</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+    `
 }
 
 loadPhotos();
