0a1860ecf4f56eecc91425f6c6d73af4a0c8e95b
saidulcoder <isaidulislamsayed@gmail.com>
2023-02-21 23:51:02 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,21 @@ const loadPhotos = () => {
 const displayPhotos = photos=>{
     photos.forEach(photo => {
         console.log(photo)
+        const allPhotoDiv = document.getElementById('photo-card');
+        const photoDiv = document.createElement('div');
+        photoDiv.innerHTML = `
+        <div class="col">
+        <div class="card">
+          <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${photo.title}</h5>
+            <p class="card-text">Album ID: ${photo.albumId}</p>
+            <button class="btn btn-primary" id="show-btn">Show Detials</button>
+          </div>
+        </div>
+      </div>
+        `;
+        allPhotoDiv.appendChild(photoDiv);
     });
 }
 
